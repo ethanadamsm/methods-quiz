44ca65e86d96595790b88ec56cdca3cef6f52f75
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-09-02 09:58:07 -0600

    icy_hot complete

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -17,7 +17,13 @@ module MethodsQuiz
 		end
 	end
 	
-	# TODO - write icy_hot?
+	def icy_hot(a, b)
+		if a < 0 && b > 100 || a > 100 && b < 0
+			true
+		else
+			false
+		end
+	end
 	
 	# TODO - write closer_to
 
