a445d5ec5c5770906f35e34bf48f7838b18fa827
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-09-02 09:49:06 -0600

    has_teen complete

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,12 @@
 module MethodsQuiz
 	
-	# TODO - write has_teen?
+	def has_teen?(a, b, c)
+		if a >= 13 && a <= 19 || b >= 13 && b <= 19 || c >= 13 && c <= 19
+			true
+		else
+			false
+		end
+	end
 	
 	# TODO - write not_string
 	
